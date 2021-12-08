81971b80df94f3c251b07aff031c6a7195ad751a
Bintou Sanogo <bintou123-ski@github.com>
2021-12-09 01:25:41 +0800

    Calculate mostUsed and most Popular language,stars and forks

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -7,6 +7,49 @@ import Followers from "./Followers";
 
 const Repos = () =>{
   const {repos} = React.useContext(GithubContext);
+const languages = repos.reduce((total,item)=>{
+const{language,stargazers_count} = item;
+if(!language){
+  return total;
+}
+if(!total[language]){
+return total[language] = {label:language, value:1,stars:stargazers_count }
+}
+else{
+total[language] = {...total[language],
+   value:total[language].value+1,
+    stars:total[language].stars + stargazers_count}
+}
+},{});
+
+const mostUsed = Object.values(languages)
+.sort((a,b)=>{
+return b.value - a.value;
+});
+
+// most  stars per language
+const mostPopular = Object.values(languages)
+.sort((a,b)=>{
+  return b.stars - a.stars;
+})
+.map((item)=>{
+  return { ...item, value:item.stars}
+})
+.slice(0,5);
+
+// stars, forks
+
+let{stars,forks} = repos.reduce(
+(total,item)=>{
+const{stargazers_count,name,forks} = item;
+total.stars[stargazers_count] = {label:name, value:stargazers_count};
+return total;
+},{
+ stars:{}, forks:{} 
+});
+
+stars = Object.values(stars).slice(-5).reverse();
+forks = Object.values(forks).slice(-5).reverse();
   return(
     <section className="section">
       <Wrapper className="section-center">
@@ -14,7 +57,6 @@ const Repos = () =>{
          <Doughnut2D/>
          <Column3D/>
          <Bar3D/>
-        
     </Wrapper>
     </section>
   )
